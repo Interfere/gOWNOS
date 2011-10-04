28989a3c1dc97e3f4f57fbaf3507de3f0167f64b
Alexey Komnin <interfere.work@gmail.com>
2011-10-04 11:23:29 +0400

    Lesson 3. Prerequisites

--- src/main.c
+++ src/main.c
@@ -1,9 +1,14 @@
 // main.c -- Defines the C-code kernel entry point, calls initialisation routines.
 
 
-int kmain(struct multiboot *mboot_ptr)
+void kmain(int magic, struct multiboot *mboot_ptr)
 {
+	// Check for multiboot magic
+	if(magic != 0x2BADB002)
+	{
+		// error. Bootloader not multiboot-compliant
+		return;
+	}
 	// All our initialisation calls will go in here
-	return 0xDEADBABA;
 }
 
